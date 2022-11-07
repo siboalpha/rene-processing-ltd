8410819f88531f32355901f717ccd20cb3e4c325
siboalpha <sibo.alphonsee@gmail.com>
2022-11-07 11:36:15 +0200

    added brand lebels to admin

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Rene Processing Ltd Admin"
+admin.site.site_title = "Rene Processing Ltd Admin Portal"
+admin.site.index_title = "Welcome to Rene Processing Ltd Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
