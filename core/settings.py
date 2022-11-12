5235928f39da30da1d100f44d3af3b0692a987f5
siboalpha <sibo.alphonsee@gmail.com>
2022-11-12 10:40:36 +0200

    corrected typos and hidden secret key

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+#load_dotenv()
+load_dotenv("/home/admin/rene-processing-ltd/core/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8=l@(mg*x(-2t#*q$few0+ll**-1tj_!-263*qbk68fy(tac3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['reneprocessing.com', 'www.reneprocessing.com', '127.0.0.1']
 
